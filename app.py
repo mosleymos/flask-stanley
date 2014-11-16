b53ea3dcd10e367c1f23fe691bbc18dfd0332166
Mori <keitamori@gmail.com>
2014-11-16 14:14:56 +0100

    Ajout d'une route

--- app.py
+++ app.py
@@ -13,8 +13,13 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    
+
     return render_template('pages/index.html')
+
+
+@app.route('/tell_me_about_your_movie')
+def tell_me_about_your_movie():
+    return render_template('pages/tell_me_about_your_movie.html')
 
 #-------------------------------------------------------------------------------
 port = os.getenv('VCAP_APP_PORT', '5000')
